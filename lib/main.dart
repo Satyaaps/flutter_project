828d9a012c1eec10f944adf33933d1e285c92567
Satya <satyaariya71@gmail.com>
2024-04-14 08:48:34 +0800

    Adding staging project

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart'; // Impor halaman LoginPage
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: LoginPage(), // Gunakan LoginPage sebagai halaman utama
+  ));
+}
